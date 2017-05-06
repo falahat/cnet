a29c8bddca9977f3c4cf5f1e7c3c85867e3d4fd0
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2017-05-06 16:40:44 -0700

    Added doc latex output

--- docs/html/search/all_b.js
+++ docs/html/search/all_b.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['scale',['scale',['../class_a_f_matrix.html#a905b58769fceaf03972f7ea179ea3934',1,'AFMatrix']]],
-  ['subtract',['subtract',['../class_a_f_matrix.html#a8826fb01197325729f0516721e14027f',1,'AFMatrix']]]
+  ['subtract',['subtract',['../class_a_f_matrix.html#a85ffed84dc6b1acb24a2c73e54511dff',1,'AFMatrix']]]
 ];
